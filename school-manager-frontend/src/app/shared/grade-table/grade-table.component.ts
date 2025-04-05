659f50f89bf5295b99a644f4019a4132d4f38061
RalucaSpt <spatarularisaraluca@gmail.com>
2025-04-05 21:59:27 +0300

    SMS-117 Add new components

---
+++ school-manager-frontend/src/app/shared/grade-table/grade-table.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-grade-table',
+  imports: [],
+  templateUrl: './grade-table.component.html',
+  styleUrl: './grade-table.component.css'
+})
+export class GradeTableComponent {
+
+}
