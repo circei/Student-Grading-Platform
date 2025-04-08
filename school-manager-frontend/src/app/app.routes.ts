910b837937fdfb68406c634e42b8efde5489dafe
RalucaSpt <spatarularisaraluca@gmail.com>
2025-04-08 19:14:44 +0300

    SMS-122: Add teacher dashboard with classes and recent grades sections

--- school-manager-frontend/src/app/app.routes.ts
+++ school-manager-frontend/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
 import { ForgotPasswordComponent } from './auth/forgot-password/forgot-password.component';
 import { AdminComponent } from './admin/admin.component';
+import { TeacherDashboardComponent } from './teacher/teacher-dashboard/teacher-dashboard.component';
 
 export const routes: Routes = [
-    {path: '', redirectTo: 'admin', pathMatch: 'full'},
-    { path: 'auth', component: AuthComponent },
-    {path: 'forgot-password', component: ForgotPasswordComponent},
-    { path: 'admin', component: AdminComponent}
+  { path: '', redirectTo: 'teacher-dashborad', pathMatch: 'full' },
+  { path: 'auth', component: AuthComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
+  { path: 'admin', component: AdminComponent },
+  { path: 'teacher-dashborad', component: TeacherDashboardComponent },
 ];
--- school-manager-frontend/src/app/teacher/teacher-dashboard/teacher-dashboard.component.ts
+++ school-manager-frontend/src/app/teacher/teacher-dashboard/teacher-dashboard.component.ts
@@ -1,11 +1,20 @@
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-teacher-dashboard',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './teacher-dashboard.component.html',
   styleUrl: './teacher-dashboard.component.css'
 })
 export class TeacherDashboardComponent {
-
+  public teacherName: string | undefined = 'John Doe';
+  public classes: { id: number; name: string; studentCount: number }[] = [
+    { id: 1, name: 'Math 101', studentCount: 25 },
+    { id: 2, name: 'Physics 201', studentCount: 30 },
+    { id: 3, name: 'Chemistry 301', studentCount: 20 }
+  ];
+  public recentGrades: { id: number; studentName: string; value: number; course: string, date: Date }[] = [
+    { id: 1, studentName: 'Alice Johnson', value: 95, course: 'Math 101', date: new Date('2023-10-01') },
+  ]
 }
