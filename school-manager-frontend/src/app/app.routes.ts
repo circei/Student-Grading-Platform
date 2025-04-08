8413ced64e33fa6f935ad220c11811eb75a9781c
RalucaSpt <spatarularisaraluca@gmail.com>
2025-04-08 19:29:07 +0300

    SMS-122: Implement class management features with add and delete functionality

--- school-manager-frontend/src/app/app.routes.ts
+++ school-manager-frontend/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { AuthComponent } from './auth/auth.component';
 import { ForgotPasswordComponent } from './auth/forgot-password/forgot-password.component';
 import { AdminComponent } from './admin/admin.component';
 import { TeacherDashboardComponent } from './teacher/teacher-dashboard/teacher-dashboard.component';
+import { ClassManagementComponent } from './teacher/class-management/class-management.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'teacher-dashborad', pathMatch: 'full' },
+  { path: '', redirectTo: 'class-management', pathMatch: 'full' },
   { path: 'auth', component: AuthComponent },
   { path: 'forgot-password', component: ForgotPasswordComponent },
   { path: 'admin', component: AdminComponent },
   { path: 'teacher-dashborad', component: TeacherDashboardComponent },
+  { path: 'class-management', component: ClassManagementComponent}
 ];
