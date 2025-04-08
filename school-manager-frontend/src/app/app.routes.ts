194b3f9be8f12c83b028b0d892ade610df169b5f
RalucaSpt <spatarularisaraluca@gmail.com>
2025-04-08 18:45:12 +0300

    SMS-122: Implement admin panel with student and professor management features

--- school-manager-frontend/src/app/app.routes.ts
+++ school-manager-frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
 import { ForgotPasswordComponent } from './auth/forgot-password/forgot-password.component';
+import { AdminComponent } from './admin/admin.component';
 
 export const routes: Routes = [
-    {path: '', redirectTo: 'auth', pathMatch: 'full'},
+    {path: '', redirectTo: 'admin', pathMatch: 'full'},
     { path: 'auth', component: AuthComponent },
     {path: 'forgot-password', component: ForgotPasswordComponent},
+    { path: 'admin', component: AdminComponent}
 ];
